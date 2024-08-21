5084473f4a9921299d2aaa0548da1b0c3b97201c
+ <+>
2024-08-21 09:31:50 +0300

    refactor:add download option for the resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -77,9 +77,10 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://my-cv-livid-nu.vercel.app/"
+          href="Resume.pdf"
+          download="Resume.pdf"
         >
-          See cv
+          Download Resume
         </a>
 
         <a
