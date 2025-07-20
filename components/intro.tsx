a1616a1d7b92843defdab31d52182e4ad1669cf5
+ <+>
2025-07-20 13:51:56 +0300

    fix:fix static files

--- components/intro.tsx
+++ components/intro.tsx
@@ -70,14 +70,14 @@ export default function Intro() {
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-
+{/* 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="Resume.pdf"
           download="Resume.pdf"
         >
           Download Resume
-        </a>
+        </a> */}
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
